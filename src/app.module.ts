0db5c81a4a03eea0ee4ce64e733fe9a75d685756
reisdiego42 <reisdiego144@gmail.com>
2023-10-24 20:30:49 -0300

    adicionando arquivo de configuraçao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { DomainModule } from 'src/domain'
 import { User } from 'src/domain/user/entities'
@@ -16,6 +17,7 @@ import { User } from 'src/domain/user/entities'
             synchronize: true,
         }),
         DomainModule,
+        ConfigModule.forRoot(),
     ],
 })
 export class AppModule {}
