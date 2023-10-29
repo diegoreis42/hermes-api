3569a4955864b5420c6668adb9772fbd26b79350
reisdiego42 <reisdiego144@gmail.com>
2023-10-28 21:30:38 -0300

    retirando senha do token!

--- src/domain/auth/usecases/auth.usecase.ts
+++ src/domain/auth/usecases/auth.usecase.ts
@@ -3,7 +3,7 @@ import { IAuthService, IAuthUseCases } from 'src/domain/auth/interfaces';
 import { RegisterUserDto, UserCredentialsDto } from 'src/domain/user/dtos';
 import { IUsersRepository, IUsersServices } from 'src/domain/user/interfaces';
 import * as bcrypt from 'bcrypt';
-import { AuthEnum } from 'src/domain/auth/enums';
+import { AuthEnum, AuthErrorsEnum } from 'src/domain/auth/enums';
 
 @Injectable()
 export class AuthUseCases implements IAuthUseCases {
@@ -16,7 +16,7 @@ export class AuthUseCases implements IAuthUseCases {
     async register(user: RegisterUserDto) {
         await this.usersService.verifyEmailExists(user.email);
 
-        const newUser = await this.usersRepository.createOne({
+        const { password, ...newUser } = await this.usersRepository.createOne({
             ...user,
             password: await bcrypt.hash(
                 user.password,
@@ -28,10 +28,13 @@ export class AuthUseCases implements IAuthUseCases {
     }
 
     async login(user: UserCredentialsDto) {
-        const authUser = await this.usersService.findByEmail(user.email);
-        if (await bcrypt.compare(user.password, authUser.password))
+        const {password, ...authUser} = await this.usersService.findByEmail(user.email);
+        if (await bcrypt.compare(user.password, password))
             return this.authService.createAccessToken(authUser);
 
-        throw new HttpException('Senha incorreta', HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+            AuthErrorsEnum.WRONG_PASSWORD,
+            HttpStatus.BAD_REQUEST
+        );
     }
 }
--- src/domain/user/dtos/index.ts
+++ src/domain/user/dtos/index.ts
@@ -1,3 +1,4 @@
 export * from './credentials-user.dto';
 export * from './register-user.dto';
+export * from './return-user.dto';
 export * from './user.dto';
