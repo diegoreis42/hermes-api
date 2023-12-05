e66541ce5025de4af73f4433c2bc5f093375518d
Diego Reis <79876389+diegoreis42@users.noreply.github.com>
2023-12-05 08:40:00 -0300

    implementando rota para editar usuario (#14)

--- src/domain/user/interfaces/users-services.interface.ts
+++ src/domain/user/interfaces/users-services.interface.ts
@@ -1,7 +1,7 @@
 import { User } from 'src/domain/user/entities';
 
 export abstract class IUsersServices {
-    abstract verifyEmailExists(email: string): Promise<Boolean>;
+    abstract verifyEmailExists(email: string): Promise<boolean>;
     abstract findByEmail(email: string): Promise<User>;
     abstract findById(id: number): Promise<User>;
 }
